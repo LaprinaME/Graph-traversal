f28232214f79c3e23cbb41ee28db6aab2f8396c3
LaprinaME <112739124+LaprinaME@users.noreply.github.com>
2023-05-16 11:29:47 +0700

    Добавьте файлы проекта.

---
+++ Graph traversal/Graph.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_traversal
+{
+    public class Graph
+    {
+        private int V; // Количество вершин
+        private List<int>[] adj; // Список смежности
+
+        public Graph(int v)
+        {
+            V = v;
+            adj = new List<int>[V]; // Создаем массив списков смежности размером V
+            for (int i = 0; i < V; i++)
+            {
+                adj[i] = new List<int>(); // Инициализируем каждый список смежности
+            }
+        }
+
+        public void AddEdge(int v, int w)
+        {
+            adj[v].Add(w); // Добавляем ребро между вершинами v и w, добавляя w в список смежности вершины v
+        }
+
+        public void BFS(int startVertex)
+        {
+            bool[] visited = new bool[V]; // Массив для отслеживания посещенных вершин
+            Queue<int> queue = new Queue<int>(); // Очередь для обхода в ширину
+
+            visited[startVertex] = true; // Помечаем начальную вершину как посещенную
+            queue.Enqueue(startVertex); // Добавляем начальную вершину в очередь
+
+            while (queue.Count != 0)
+            {
+                startVertex = queue.Dequeue(); // Извлекаем вершину из очереди
+                Console.Write(startVertex + " "); // Выводим вершину
+
+                foreach (int adjacentVertex in adj[startVertex]) // Для каждой смежной вершины
+                {
+                    if (!visited[adjacentVertex]) // Если вершина не была посещена
+                    {
+                        visited[adjacentVertex] = true; // Помечаем ее как посещенную
+                        queue.Enqueue(adjacentVertex); // Добавляем в очередь для дальнейшего обхода
+                    }
+                }
+            }
+        }
+
+        private void DFSUtil(int v, bool[] visited)
+        {
+            visited[v] = true; // Помечаем текущую вершину как посещенную
+            Console.Write(v + " "); // Выводим вершину
+
+            foreach (int adjacentVertex in adj[v]) // Для каждой смежной вершины
+            {
+                if (!visited[adjacentVertex]) // Если вершина не была посещена
+                {
+                    DFSUtil(adjacentVertex, visited); // Рекурсивно вызываем DFSUtil для смежной вершины
+                }
+            }
+        }
+
+        public void DFS(int startVertex)
+        {
+            bool[] visited = new bool[V]; // Массив для отслеживания посещенных вершин
+            DFSUtil(startVertex, visited); // Вызываем вспомогательный метод DFSUtil для обхода в глубину
+        }
+
+        public void PrintAdjacencyMatrix()
+        {
+            Console.WriteLine("Таблица смежности вершин:");
+            for (int i = 0; i < V; i++)
+            {
+                Console.Write(i + ": ");
+                foreach (int adjacentVertex in adj[i]) // Для каждой смежной вершины
+                {
+                    Console.Write(adjacentVertex + " "); // Выводим смежную вершину
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
+
---
+++ Graph traversal/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Graph_traversal
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int vertexCount = 5; // Количество вершин в графе
+
+            Graph graph = new Graph(vertexCount);
+
+            // Добавление ребер в граф
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(2, 4);
+            graph.AddEdge(3, 4);
+
+            Console.WriteLine("Обход в ширину (BFS):");
+            graph.BFS(0);
+            Console.WriteLine();
+
+            Console.WriteLine("\nОбход в глубину (DFS):");
+            graph.DFS(0);
+            Console.WriteLine();
+
+            graph.PrintAdjacencyMatrix();
+        }
+    }
+}
